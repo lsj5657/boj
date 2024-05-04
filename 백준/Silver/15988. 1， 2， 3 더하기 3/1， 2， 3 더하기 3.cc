85460fc8c9de9a5fcf36983dc08e744eee4857f7
lsj5657 <109152587+lsj5657@users.noreply.github.com>
2024-05-04 12:16:38 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 16 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define ll long long 
 
-int n;
+int MAX = 1000000;
 ll mod = 1e9 + 9;
 ll dp[1000006];
 
+
 int main() {
-	cin.tie(0);
+	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(0);
+
 	dp[1] = 1;
 	dp[2] = 2;
 	dp[3] = 4;
-	for (int i = 4; i <= 1000000; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-		dp[i] %= mod;
+	
+	for(int i = 4; i <= MAX; i++){
+		dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % mod;
 	}
+
 	int tc;
 	cin >> tc;
-	while (tc--) {
+	while(tc--){
+		int n;
 		cin >> n;
 		cout << dp[n] << '\n';
 	}
 
-
-	
 }
+
 
